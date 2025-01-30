4943f16709358acdf1830c5bf91c15f996c274b9
AneshKarki <aneshkarki07@gmail.com>
2025-01-30 09:38:26 +0545

    Server Setup

--- client/vizonX/vite.config.js
+++ client/vizonX/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import process from "process";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 4173, // Default to 3000 if PORT is not defined
+    host: "0.0.0.0", // Allows access from other devices in the network
+  },
+});
